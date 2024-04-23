55659951bb192df64d85bcc19c5f25b56298a7df
Isaac Anderson <95647478+IsaacAnderson1@users.noreply.github.com>
2024-04-22 22:29:31 -0500

    Add files via upload

---
+++ WarGameDemo/WarGameDemo/WarGameDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WarGameDemoApp.swift
+//  WarGameDemo
+//
+//  Created by 64000114 on 2/7/24.
+//
+
+import SwiftUI
+
+@main
+struct WarGameDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
